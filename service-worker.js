63363cbc6fcfdadf8cbba2f7bbba4c513a4f977a
Hayaokuri <58360045+hayaokuri@users.noreply.github.com>
2026-02-16 07:35:36 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,12 @@
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("habit-cache").then(cache =>
-      cache.addAll(["./", "./index.html"])
+      cache.addAll([
+        "./", 
+        "./index.html",
+        "./images/icon-192.png",  // ← 追加
+        "./images/icon-512.png"   // ← 追加
+      ])
     )
   );
 });
